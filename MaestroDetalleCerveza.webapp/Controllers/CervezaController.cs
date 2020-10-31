51864848e74b1d17db6fa96d4ab0b293038b7b26
gcarlossuarez <70105680+gcarlossuarez@users.noreply.github.com>
2020-10-30 21:07:57 -0400

    Edit, con Index que carga listado

--- MaestroDetalleCerveza.webapp/Controllers/CervezaController.cs
+++ MaestroDetalleCerveza.webapp/Controllers/CervezaController.cs
@@ -5,12 +5,87 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 using MaestroDetalleCerveza.webapp.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
 //using Cerveza.api.Controllers;
 
 namespace MaestroDetalleCerveza.webapp.Controllers
 {
     public class CervezaController : Controller
     {
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            using (var DB = new Cerveza.api.Models.MaestroDetalleContext())
+            {
+                using (var l_Transacción = DB.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        // Para no hacerlo muy pesado, no se carga el detalle; se cargará por demanda
+                        //var l_ListCerveza = 
+                        //    await DB.Cerveza.Select(x=> 
+                        //       CervezaController.TransformarCervezaEnCervezaEditViewModel(x, DB, true)).ToListAsync();
+                        var l_ListCerveza =
+                            await DB.Cerveza.Select(x =>
+                               CervezaController.TransformarCervezaEnCervezaEditViewModel(x, DB, false)).ToListAsync();
+
+                        l_Transacción.Commit();
+
+                        return View(l_ListCerveza);
+                    }
+                    catch (Exception ex)
+                    {
+                        l_Transacción.Rollback();
+
+                        return View("Error", "Models");
+                    }
+                    
+                }
+                //return View(await DB.Cerveza.ToListAsync()); //.ToListAsync());
+            }
+            
+        }
+        
+        private static CervezaEditViewModel TransformarCervezaEnCervezaEditViewModel(Cerveza.api.Models.Cerveza p_Cerveza,
+                                                                 Cerveza.api.Models.MaestroDetalleContext p_MaestroDetalleContext,
+                                                                 bool p_CargarIngredientesSiNoEstuvieranCargados)
+        {
+            CervezaEditViewModel l_CervezaEditViewModel = new CervezaEditViewModel()
+            {
+                Id = p_Cerveza.Id,
+                Estilo = p_Cerveza.Estilo,
+                Nombre = p_Cerveza.Nombre
+            };
+
+            List<Cerveza.api.Models.Ingrediente> l_ListIngredientes = p_Cerveza.Ingrediente.ToList();
+            if( (p_Cerveza.Ingrediente.Count == 0) && (p_CargarIngredientesSiNoEstuvieranCargados))
+            {
+                l_ListIngredientes = new List<Cerveza.api.Models.Ingrediente>();
+
+                // NTOA.- El '30/10/2020', se caía si no cerraba el "DataReader" original
+                //l_ListIngredientes = p_MaestroDetalleContext.Ingrediente.Where(x => x.IdCerveza == p_Cerveza.Id).ToList();
+                using (var DB = new Cerveza.api.Models.MaestroDetalleContext())
+                {
+                    l_ListIngredientes = DB.Ingrediente.Where(x => x.IdCerveza == p_Cerveza.Id).ToList();
+                }
+            }
+            l_CervezaEditViewModel.ingredientes = new List<IngredienteEditViewModel>();
+            foreach (var l_IngerienteModelDB in l_ListIngredientes)
+            {
+                IngredienteEditViewModel l_IngredienteEditViewModel = new IngredienteEditViewModel
+                {
+                    Id = l_IngerienteModelDB.Id,
+                    NombreIngrediente = l_IngerienteModelDB.Nombre,
+                    Gramos = l_IngerienteModelDB.Gramos,
+                    IdCerveza = l_IngerienteModelDB.IdCerveza
+                };
+                l_CervezaEditViewModel.ingredientes.Add(l_IngredienteEditViewModel);
+            }
+
+            return l_CervezaEditViewModel;
+        }
+        
+
         [HttpGet]
         public IActionResult Create()
         {
---
+++ MaestroDetalleCerveza.webapp/Models/ViewModels/CervezaEditViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MaestroDetalleCerveza.webapp.Models.ViewModels
+{
+    public class CervezaEditViewModel
+    {
+        public int Id {get; set;}
+        public string Nombre { get; set; }
+        public string Estilo { get; set; }
+
+        public List<IngredienteEditViewModel> ingredientes { get; set; }
+    }
+
+    public class IngredienteEditViewModel
+    {
+        public int Id { get; set; }
+        public string NombreIngrediente { get; set; }
+        public int Gramos { get; set; }
+
+        public int IdCerveza { get; set; }
+    }
+}
